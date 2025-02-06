256b1bd1e45d3de9ca3475138acff71055265582
wornbr22p@outlook.com <wornbr22p@outlook.com>
2025-02-06 19:25:20 -0300

    validates data before api call

--- backend/routes/scannerRoutes.js
+++ backend/routes/scannerRoutes.js
@@ -1,9 +1,26 @@
 // backend/routes/scannerRoutes.js
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
-
 const { analyzeCode } = require('../controllers/scannerController');
 
-router.post('/analyze', analyzeCode);
+router.post(
+    '/analyze', 
+    [
+        body('code')
+        .exists().withMessage('Código é obrigatório.')
+        .isString().withMessage('Código deve ser uma string.')
+        .trim()
+        .notEmpty().withMessage('Código não pode estar vazio.')
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+    analyzeCode
+);
 
 module.exports = router;
