d55ead6035000a5c4fd8b95958ff4ad9d5aa8f00
wornbr22p@outlook.com <wornbr22p@outlook.com>
2025-02-06 19:34:33 -0300

    added file upload option

--- frontend/src/CodeScanner.jsx
+++ frontend/src/CodeScanner.jsx
@@ -7,6 +7,20 @@ const CodeScanner = () => {
   const [analysis, setAnalysis] = useState("");
   const [loading, setLoading] = useState(false);
 
+  // upload function
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (file && file.type === "applications/javascript" || file.name.endsWith(".js")) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setCode(event.target.result);
+      };
+      reader.readAsText(file);
+    } else {
+      alert("Por favor, selecione um arquivo válido (.js - JavaScript).");
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
@@ -26,11 +40,23 @@ const CodeScanner = () => {
   return (
     <div style={{ padding: "20px" }}>
       <h2>Detector de Scripts Maliciosos</h2>
+
+      {/** upload form file */}
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="file-upload">Upload do arquivo .js</label>
+        <input
+          id="file-upload"
+          type="file"
+          accept=".js"
+          onChange={handleFileUpload}
+        />
+      </div>
+
       <form onSubmit={handleSubmit}>
         <textarea
           rows="10"
           cols="80"
-          placeholder="Cole seu código JavaScript aqui..."
+          placeholder="Cole seu código JavaScript aqui ou carregue um arquivo .js"
           value={code}
           onChange={(e) => setCode(e.target.value)}
           required
